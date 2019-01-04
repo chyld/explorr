a2a640e55759555356c1df83ae45cedad5ee5812
Chyld Medford <chyld.medford@gmail.com>
2019-01-03 23:52:37 -0800

    mvp

---
+++ index.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+
+const fs = require("fs");
+const moment = require("moment");
+
+const lines = fs.readFileSync("./data.txt", { encoding: "utf8" });
+const options = lines.trim().split("\n");
+const count = options.length;
+const item = Math.floor(Math.random() * count);
+const date = moment().format("YYYY-MM-DD HH:mm:ss");
+const option = options[item];
+const response = `${date} >> ${option}`;
+
+console.log(response);
