ce39ac188ca912c14eed18a31f49a151f014ec14
Chyld Medford <chyld.medford@gmail.com>
2019-01-04 02:51:11 -0800

    tracking

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 const fs = require("fs");
 const moment = require("moment");
 
-const lines = fs.readFileSync("./data.txt", { encoding: "utf8" });
+const lines = fs.readFileSync("./data/in.txt", { encoding: "utf8" });
 const options = lines.trim().split("\n");
 const count = options.length;
 const item = Math.floor(Math.random() * count);
 const date = moment().format("YYYY-MM-DD HH:mm:ss");
-const option = options[item];
-const response = `${date} >> ${option}\n\nRecord upon completion.`;
+let option = options[item];
+option = option.replace(/,/gi, "-");
+const response = `${date}, ${option}, TODO\n`;
 
+fs.writeFileSync("./data/out.csv", response, { flag: "a" });
 console.log(response);
